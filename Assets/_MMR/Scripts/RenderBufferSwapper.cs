5e88888f00da49d415bc830fc240bc7b5e3a5136
Mio Bambino <s59330@beuth-hochschule.de>
2017-06-23 12:45:51 +0200

    general update a Please enter the commit message for your changes. Lines starting

--- Assets/_MMR/Scripts/RenderBufferSwapper.cs
+++ Assets/_MMR/Scripts/RenderBufferSwapper.cs
@@ -4,40 +4,50 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [ExecuteInEditMode]
-public class RenderBufferSwapper : MonoBehaviour {
+public class RenderBufferSwapper : MonoBehaviour
+{
     /*
-
     public List<RenderTexture> buffers;
-
 	public int index;
 	public long seenFrames;
 	public Material cameraViewMaterial;
     public RawImage rawOutputDisplay;
-    */ 
+    */
     [RangeAttribute(15, 60)]
     public float cameraFPS;
     [RangeAttribute(0, 1)]
     public float cameraOffset;
     public Material targetMaterial;
-    public Camera observerCamera;
+    public Camera fullCamera;
     public Camera frontCamera;
+    public Camera stencilCamera;
+    public Camera lightCamera;
+    public string materialFullFieldName;
+    public string materialFrontFieldName;
+    public string materialWebcamFieldName;
+    public string materialWebcamMaskFieldName;
+    public string materialLightFieldName;
 
 
     private float frameWindow;
     private float delay;
-    private int IntDelay { get { return (int)delay;} }
+    private int IntDelay { get { return (int)delay; } }
     private float frameDelay;
-    private int IntFrameDelay { get { return (int)frameDelay;} }
+    private int IntFrameDelay { get { return (int)frameDelay; } }
     private float fractionDelay;
     private float innerTimer;
     private float absoluteTimer;
     private float initialDelay;
     public List<RenderTexture> colorBuffers;
     public List<RenderTexture> alphaBuffers;
+    public List<RenderTexture> stencilBuffers;
+    public List<RenderTexture> lightBuffers;
+    public WebcamEnabler webcamEnabler;
     public int index;
 
 
-    void Start () {
+    void Start()
+    {
         frameWindow = 1.0f / cameraFPS;
         delay = cameraOffset / frameWindow;
         frameDelay = (int)delay * frameWindow;
@@ -47,46 +57,80 @@ public class RenderBufferSwapper : MonoBehaviour {
         absoluteTimer = 0.0f;
         initialDelay = frameDelay + fractionDelay;
 
-		RebuildRenderBuffers();
-	}
-	
-	void Update () {
+        RebuildRenderBuffers();
+
+        targetMaterial.SetTexture(materialWebcamFieldName, webcamEnabler.webcamTexture);
+    }
+
+    void Update()
+    {
         innerTimer += Time.deltaTime;
         absoluteTimer += Time.deltaTime;
         var localTime = innerTimer - fractionDelay;
-        if(localTime < frameWindow || absoluteTimer < initialDelay) {
+        if (localTime < frameWindow || absoluteTimer < initialDelay)
+        {
             return;
         }
 
         SwapRenderBuffer();
         innerTimer %= frameWindow;
         absoluteTimer = absoluteTimer % (2f) + initialDelay;
-	}
+    }
 
-    void SwapRenderBuffer() {
+    void SwapRenderBuffer()
+    {
         index = index % IntDelay;
-        //  observerCamera.SetTargetBuffers(colorBuffers[index].colorBuffer, depthBuffers[index].depthBuffer);
-        observerCamera.targetTexture = colorBuffers[index];
+        //  fullCamera.SetTargetBuffers(colorBuffers[index].colorBuffer, depthBuffers[index].depthBuffer);
+        fullCamera.targetTexture = colorBuffers[index];
         frontCamera.targetTexture = alphaBuffers[index];
+        stencilCamera.targetTexture = stencilBuffers[index];
+        lightCamera.targetTexture = lightBuffers[index];
+        // stencilCamera.SetTargetBuffers(sBuf.colorBuffer, sBuf.depthBuffer);
+
+
         var frameTex = colorBuffers[(index + 1) % IntDelay];
         var alphaTex = alphaBuffers[(index + 1) % IntDelay];
-        targetMaterial.SetTexture("_MainTex", frameTex);
-        targetMaterial.SetTexture("_AlphaTex", alphaTex);
+        var stencilTex = stencilBuffers[(index + 1) % IntDelay];
+        var lightTex = lightBuffers[(index + 1) % IntDelay];
+
+        // at this point do the stencil operation on the WC material
+        // stencilMaterial.mainTexture = stencilTex;
+        // Graphics.Blit(stencilTex, stencilTex, stencilMaterial);
+        // unlitDebugDisplay.mainTexture = stencilTex;
+        targetMaterial.SetTexture(materialFullFieldName, frameTex);
+        targetMaterial.SetTexture(materialFrontFieldName, alphaTex);
+        targetMaterial.SetTexture(materialWebcamMaskFieldName, stencilTex);
+        targetMaterial.SetTexture(materialLightFieldName, lightTex);
         index++;
     }
 
-	void RebuildRenderBuffers() {
+    void RebuildRenderBuffers()
+    {
         colorBuffers = new List<RenderTexture>();
         alphaBuffers = new List<RenderTexture>();
-		for(int i = 0; i < IntDelay; i++) {
+        stencilBuffers = new List<RenderTexture>();
+        lightBuffers = new List<RenderTexture>();
+        for (int i = 0; i < IntDelay; i++)
+        {
             var cBuf = new RenderTexture(Screen.width, Screen.height, 32, RenderTextureFormat.ARGB32);
+            cBuf.name = "Color Buffer " + i;
             // cBuf.antiAliasing = QualitySettings.antiAliasing;
             colorBuffers.Add(cBuf);
 
-            var aBuf = new RenderTexture(Screen.width, Screen.height, 32, RenderTextureFormat.ARGB32);
+
+            var aBuf = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32);
+            aBuf.name = "Front Buffer " + i;
             // aBuf.antiAliasing = QualitySettings.antiAliasing;
             alphaBuffers.Add(aBuf);
-		}
-		Debug.Log("Rebuilt buffers");
-	}
+
+            var sBuf = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32);
+            sBuf.name = "Stencil Buffer " + i;
+            stencilBuffers.Add(sBuf);
+
+            var lBuf = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32);
+            lBuf.name = "Light Buffer " + i;
+            lightBuffers.Add(lBuf);
+        }
+        Debug.Log("Rebuilt buffers");
+    }
 }
--- Assets/_MMR/Scripts/WebcamEnabler.cs
+++ Assets/_MMR/Scripts/WebcamEnabler.cs
@@ -3,92 +3,95 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor;
-using Utilities;
 
-public class WebcamEnabler : MonoBehaviour {
-
-	private static string[] _camNames;
-	public static string[] CamNames {
-		get {
-			if(_camNames == null) {
-				ReloadCameras();
-			}
-			return _camNames;
-		}
-	}
-	public static void ReloadCameras() {
-		List<string> container = new List<string>();
-		WebCamTexture.devices.ToList().ForEach(x => {container.Add(x.name);});
-		_camNames = container.ToArray();
-	}
-
-	[SerializeField]
-	private int _camIndex;
-	public int CamIndex {
-		get {
-			return _camIndex;
-		}
-		set {
-			if(_camIndex != value) {
-				_camIndex = value;
-				deviceName = CamNames[value];
-				ResetCamera();
-			}
-		}
-	}
-	public Material material;
-	public string deviceName;
-    public Camera cam;
-    public GameObject controller;
-    public GameObject headDisplay;
-    public float distance;
-	// public RawImage webcamUiImage;
-    // public RawImage viewportUiImage;
-
-	public Vector2 imageSize;
-	public int frameRate;
-
-	private WebCamTexture wc;
-
-	// Use this for initialization
-	void Start () {
-		if(imageSize.x == 0 && imageSize.y == 0) {
-			imageSize = new Vector2(1280, 720);
-		}
-		ResetCamera();
-	}
-
-	void OnDisable() {
-		if(wc != null && wc.isPlaying) {
-			wc.Stop();
-		}
-	}
-
-	void ResetCamera() {
-		if(wc != null && wc.isPlaying) {
-			wc.Stop();
-		}
-		if(!Application.isPlaying) {
-			return;
-		}
-		Debug.Log("Restarting Camera: " + deviceName);
-		wc = new WebCamTexture(deviceName, (int) imageSize.x, (int) imageSize.y, frameRate);
-		// material.mainTexture = wc;
-        material.SetTexture("_WebcamTex", wc);
-		// webcamUiImage.texture = wc;
-		wc.Play();
-	}
-
-    void Update() {
-        distance = Mathf.Abs((headDisplay.transform.position - controller.transform.position).magnitude) - 0.3f;
-        distance = Mathf.Max(0.25f, distance);
-        cam.farClipPlane = distance;
+public class WebcamEnabler : MonoBehaviour
+{
+    private static string[] _camNames;
+    public static string[] CamNames
+    {
+        get
+        {
+            if (_camNames == null)
+            {
+                ReloadCameras();
+            }
+            return _camNames;
+        }
+    }
+    public static void ReloadCameras()
+    {
+        List<string> container = new List<string>();
+        WebCamTexture.devices.ToList().ForEach(x => { container.Add(x.name); });
+        _camNames = container.ToArray();
     }
 
-    public void StopCamera() {
-		if(wc != null && wc.isPlaying) {
-			wc.Stop();
-		}
-	}
+    [SerializeField]
+    private int _camIndex;
+    public int CamIndex
+    {
+        get
+        {
+            return _camIndex;
+        }
+        set
+        {
+            if (_camIndex != value)
+            {
+                _camIndex = value;
+                deviceName = CamNames[value];
+                ResetCamera();
+            }
+        }
+    }
+    public Material material;
+    public string shaderPropertyName = "_MainTex";
+    public string deviceName;
+
+    public Vector2 imageSize;
+    public int frameRate;
+
+    public WebCamTexture webcamTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (imageSize.x == 0 && imageSize.y == 0)
+        {
+            imageSize = new Vector2(1280, 720);
+        }
+        ResetCamera();
+    }
+
+    void OnDisable()
+    {
+        if (webcamTexture != null && webcamTexture.isPlaying)
+        {
+            webcamTexture.Stop();
+        }
+    }
+
+    void ResetCamera()
+    {
+        if (webcamTexture != null && webcamTexture.isPlaying)
+        {
+            webcamTexture.Stop();
+        }
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+        Debug.Log("Restarting Camera: " + deviceName);
+        webcamTexture = new WebCamTexture(deviceName, (int)imageSize.x, (int)imageSize.y, frameRate);
+        webcamTexture.name = "Webcam Texture (generated)";
+        webcamTexture.Play();
+        material.SetTexture(shaderPropertyName, webcamTexture);
+    }
+
+    public void StopCamera()
+    {
+        if (webcamTexture != null && webcamTexture.isPlaying)
+        {
+            webcamTexture.Stop();
+        }
+    }
 }
